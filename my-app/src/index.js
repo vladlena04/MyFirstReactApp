df0a95fe7fa2da5815e19f48fd4723ae00c5e727
V.Bonk <vladlena.bonk@kaspersky.com>
2022-01-08 18:56:37 +0300

    add buttons

---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDom from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter'
+
+const App = () => {
+    return <Counter />
+}
+
+reactDom.render(<App />, document.getElementById('root'))
